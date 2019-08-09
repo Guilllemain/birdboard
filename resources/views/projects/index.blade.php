f10d5c5f34538e8aa1da9237e71b50ba6ef2a4ab
Yann Guillemain <devguillemain@gmail.com>
2019-08-09 14:35:27 +0200

    Setting up phpunit

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
+
+<body>
+    <ul>
+        @foreach($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
